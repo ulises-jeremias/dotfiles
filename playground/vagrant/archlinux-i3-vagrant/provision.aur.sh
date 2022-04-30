d4cad5e2a7227e51de5c1d7f5add50b2bf927fee
ulises-jeremias <ulisescf.24@gmail.com>
2022-04-30 02:17:11 -0300

    Fixed

--- playground/vagrant/archlinux-i3-vagrant/provision.aur.sh
+++ playground/vagrant/archlinux-i3-vagrant/provision.aur.sh
@@ -34,22 +34,6 @@ sudo pacman --noconfirm -U consolas-font-*
 cd ../
 rm -rf consolas-font/
 
-# install skype
-git clone https://aur.archlinux.org/skypeforlinux-beta-bin.git
-cd skypeforlinux-beta-bin/
-makepkg --noconfirm -si
-sudo pacman --noconfirm -U skypeforlinux-beta-bin-*
-cd ../
-rm -rf skypeforlinux-beta-bin/
-
-# install slack
-git clone https://aur.archlinux.org/slack-desktop.git
-cd slack-desktop/
-makepkg --noconfirm -si
-sudo pacman --noconfirm -U slack-desktop-*
-cd ../
-rm -rf slack-desktop/
-
 # install visual studio code
 git clone https://aur.archlinux.org/visual-studio-code.git
 cd visual-studio-code/
