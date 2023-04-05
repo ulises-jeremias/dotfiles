4ef31c4cc8636ed0e2ceab973d68c9e3079eb73b
ulises-jeremias <ulisescf.24@gmail.com>
2023-04-05 02:07:10 -0300

    Updated base config

--- common/config/openbox/autostart.sh
+++ common/config/openbox/autostart.sh
@@ -5,4 +5,31 @@ export XDG_CONFIG_HOME
 
 [[ "${PATH}" == *"${HOME}/.local/bin"* ]] || export PATH="${HOME}/.local/bin:${PATH}"
 
-dots-scripts autostart &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+gnome-keyring-daemon --start --components=pkcs11 &
+xset dpms 600 900 1200
+xset r rate 350 60
+
+# required for xfce settings to work
+# run xfsettingsd in the background if it's not already running
+[[ -z "$(pgrep xfsettingsd)" ]] && xfsettingsd &
+
+# load X params required for dotfiles to work
+xrdb -I"${HOME}" -load ~/.Xresources &
+
+# load picom
+[[ -z "$(pgrep picom)" ]] && picom --config "${XDG_CONFIG_HOME}"/picom.conf &
+
+# restore last configured wallpaper and colors
+dots-scripts wall-d -f -r &
+wal -R -q
+dots-scripts feh-blur --blur 10 -d &
+
+# open polybar
+"${XDG_CONFIG_HOME}"/polybar/launch &
+
+# Start greenclip daemon
+[[ -z "$(pgrep greenclip)" ]] && greenclip daemon &
+
+# Start nm-applet
+[[ -z "$(pgrep nm-applet)" ]] && nm-applet &
