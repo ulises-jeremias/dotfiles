f1d332dcfca696e914469a7b7bb1c6d145d59d9f
ulises-jeremias <ulisescf.24@gmail.com>
2023-04-23 19:54:17 -0300

    Quick fix

--- common/config/openbox/autostart.sh
+++ common/config/openbox/autostart.sh
@@ -1,46 +1,38 @@
 #!/usr/bin/env bash
-
 XDG_CONFIG_HOME="${HOME}"/.config
 export XDG_CONFIG_HOME
 
 [[ "${PATH}" == *"${HOME}/.local/bin"* ]] || export PATH="${HOME}/.local/bin:${PATH}"
 
-run() {
-  if [[ -n "$(pgrep "$1")" ]]; then
-    # shellcheck disable=SC2068
-    $@ &
-  fi
-}
-
-run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
-run gnome-keyring-daemon --start --components=pkcs11
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+gnome-keyring-daemon --start --components=pkcs11 &
 xset dpms 600 900 1200
 xset r rate 350 60
 
 # required for xfce settings to work
 # run xfsettingsd in the background if it's not already running
-run xfsettingsd
+[[ -z "$(pgrep xfsettingsd)" ]] && xfsettingsd &
 
 # run xfce4-power-manager in the background if it's not already running
-run xfce4-power-manager --daemon
+[[ -z "$(pgrep xfce4-power-manager)" ]] && xfce4-power-manager --daemon &
 
 # load X params required for dotfiles to work
-run xrdb -I"${HOME}" -load ~/.Xresources
+xrdb -I"${HOME}" -load ~/.Xresources &
 
 # load picom
-run picom --config "${XDG_CONFIG_HOME}"/picom.conf
+[[ -z "$(pgrep picom)" ]] && picom --config "${XDG_CONFIG_HOME}"/picom.conf &
 
 # restore colorscheme
-run wal -R -q
+[[ -z "$(pgrep wal)" ]] && wal -R -q
 
 # run feh-blur in the background
-run dots-scripts feh-blur --blur 10 -d
+dots-scripts feh-blur --blur 10 -d &
 
 # open polybar
-run "${XDG_CONFIG_HOME}"/polybar/launch
+"${XDG_CONFIG_HOME}"/polybar/launch &
 
 # Start nm-applet
-run nm-applet
+[[ -z "$(pgrep nm-applet)" ]] && nm-applet &
 
 # Start greenclip daemon
-run greenclip daemon
+[[ -z "$(pgrep greenclip)" ]] && greenclip daemon &
