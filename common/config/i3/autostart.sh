f92006d7059e08bc34b2c128cdc22b5bcc9fcfd9
ulises-jeremias <ulisescf.24@gmail.com>
2023-04-22 21:06:20 -0300

    Updated autostart

--- common/config/i3/autostart.sh
+++ common/config/i3/autostart.sh
@@ -5,38 +5,44 @@ export XDG_CONFIG_HOME
 
 [[ "${PATH}" == *"${HOME}/.local/bin"* ]] || export PATH="${HOME}/.local/bin:${PATH}"
 
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-gnome-keyring-daemon --start --components=pkcs11 &
-xset dpms 600 900 1200
-xset r rate 350 60
+run() {
+  if ! pgrep "$1"; then
+    "$@"&
+  fi
+}
+
+run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+run gnome-keyring-daemon --start --components=pkcs11
+run xset dpms 600 900 1200
+run xset r rate 350 60
 
 # required for xfce settings to work
 # run xfsettingsd in the background if it's not already running
-[[ -z "$(pgrep xfsettingsd)" ]] && xfsettingsd &
+run xfsettingsd
 
 # run xfce4-power-manager in the background if it's not already running
-[[ -z "$(pgrep xfce4-power-manager)" ]] && xfce4-power-manager --daemon &
+run xfce4-power-manager --daemon
 
 # load X params required for dotfiles to work
-xrdb -I"${HOME}" -load ~/.Xresources &
+run xrdb -I"${HOME}" -load ~/.Xresources
 
 # load picom
-[[ -z "$(pgrep picom)" ]] && picom --config "${XDG_CONFIG_HOME}"/picom.conf &
+run picom --config "${XDG_CONFIG_HOME}"/picom.conf
 
 # restore colorscheme
-[[ -z "$(pgrep wal)" ]] && wal -R -q
+run wal -R -q
 
 # run feh-blur in the background
-dots-scripts feh-blur --blur 10 -d &
+run dots-scripts feh-blur --blur 10 -d
 
 # open polybar
-"${XDG_CONFIG_HOME}"/polybar/launch &
+run "${XDG_CONFIG_HOME}"/polybar/launch
 
-# TODO: Make i3 autotiling work properly
-# [[ -z "$(pgrep autotiling)" ]] && "${XDG_CONFIG_HOME}"/i3/autotiling &
+# i3 autotiling
+# run ${XDG_CONFIG_HOME}/i3/autotiling
 
 # Start nm-applet
-[[ -z "$(pgrep nm-applet)" ]] && nm-applet &
+run nm-applet
 
 # Start greenclip daemon
-[[ -z "$(pgrep greenclip)" ]] && greenclip daemon &
+run greenclip daemon
