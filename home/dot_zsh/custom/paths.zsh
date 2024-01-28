0eed42d90d46ec61ceb855a4699c4849a0f378fa
ulises-jeremias <ulisescf.24@gmail.com>
2024-01-28 02:16:05 -0300

    Add paths to environment variables

--- home/dot_zsh/custom/paths.zsh
+++ home/dot_zsh/custom/paths.zsh
@@ -1,25 +1,42 @@
-# custom bin directory
-PATH="$HOME/bin:$HOME/.local/bin:$PATH"
+# add ~/bin to path if it exists and is not already in path
+[[ -d "$HOME/bin" && ! "$PATH" =~ "$HOME/bin" ]] && PATH="$HOME/bin:$PATH"
 
-# emacs bin dir
-PATH="$HOME/.emacs.d/bin:$PATH"
+# add ~/.local/bin to path if it exists and is not already in path
+[[ -d "$HOME/.local/bin" && ! "$PATH" =~ "$HOME/.local/bin" ]] && PATH="$HOME/.local/bin:$PATH"
 
-# ld
-LD_LIBRARY_PATH="/usr/local/lib:/usr/lib:${LD_LIBRARY_PATH}"
+# add ~/.emacs.d/bin to path if it exists and is not already in path
+[[ -d "$HOME/.emacs.d/bin" && ! "$PATH" =~ "$HOME/.emacs.d/bin" ]] && PATH="$HOME/.emacs.d/bin:$PATH"
 
-# yarn and fnm
-PATH="$(yarn global bin):$PATH"
-PATH="$HOME/.local/share/fnm:$HOME/.fnm:$PATH"
+# add /usr/local/bin to LD_LIBRARY_PATH if it exists and is not already in path
+[[ -d "/usr/local/lib" && ! "$LD_LIBRARY_PATH" =~ "/usr/local/lib" ]] && LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
+
+# add /usr/lib to LD_LIBRARY_PATH if it exists and is not already in path
+[[ -d "/usr/lib" && ! "$LD_LIBRARY_PATH" =~ "/usr/lib" ]] && LD_LIBRARY_PATH="/usr/lib:$LD_LIBRARY_PATH"
+
+# add yarn global bin to path if it exists and is not already in path
+YARN_GLOBAL_BIN="$(yarn global bin)"
+[[ -d "$YARN_GLOBAL_BIN" && ! "$PATH" =~ "$YARN_GLOBAL_BIN" ]] && PATH="$YARN_GLOBAL_BIN:$PATH"
+
+# add fnm directory to path if it exists and is not already in path
+FNM_DIR="$HOME/.local/share/fnm"
+[[ -d "$FNM_DIR" && ! "$PATH" =~ "$FNM_DIR" ]] && PATH="$FNM_DIR:$PATH"
+
+# add fnm directory to path if it exists and is not already in path
+FNM_DIR="$HOME/.fnm"
+[[ -d "$FNM_DIR" && ! "$PATH" =~ "$FNM_DIR" ]] && PATH="$FNM_DIR:$PATH"
+
+# eval fnm env
 eval "$(fnm env)"
 
-# pyenv
+# add pyenv directory to path if it exists and is not already in path
 export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+[[ -d $PYENV_ROOT/bin ]] && PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 
-# Golang
+# Add go bin to path
 PATH="$PATH:$(go env GOBIN):$(go env GOPATH)/bin"
 
+# Setup direnv
 eval "$(direnv hook zsh)"
 
 export LD_LIBRARY_PATH PATH GOPATH GOROOT
