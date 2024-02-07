2c84b5500fdec0bd623b7bd8e4ff8c69db66ec6e
ulises-jeremias <ulisescf.24@gmail.com>
2024-02-07 02:30:48 -0300

    Add p10k instant prompt sourcing
    This commit adds code to source the p10k instant prompt if the corresponding file is readable in the cache directory. This allows for faster prompt rendering.

--- home/dot_zsh/custom/zsh-preconfig.zsh
+++ home/dot_zsh/custom/zsh-preconfig.zsh
@@ -1,1 +1,5 @@
 ZSH_NO_DEFAULT_PROMPT=true
+
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
