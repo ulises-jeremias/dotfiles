879c9e327ae25602945cd828d4a53c4a63180955
ulises-jeremias <ulisescf.24@gmail.com>
2024-02-11 19:06:26 -0300

    Add terminal color support and remove unnecessary code***

--- home/private_dot_config/polybar/executable_launch.sh
+++ home/private_dot_config/polybar/executable_launch.sh
@@ -6,12 +6,6 @@ pkill -9 polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-if [ -z "${TERM}" ]; then
-  TERM=xterm-256color
-fi
-
-export TERM
-
 WM=$(wmctrl -m | grep -oE 'Name: .*' | cut -d' ' -f2 | tr '[:upper:]' '[:lower:]')
 
 if [ "${WM}" = "i3" ]; then
