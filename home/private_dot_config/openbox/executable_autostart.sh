d0745e4fdc3486f58e7876a08215b415512f09f3
ulises-jeremias <ulisescf.24@gmail.com>
2024-02-18 19:38:55 -0300

    Update dependencies and autostart configurations

--- home/private_dot_config/openbox/executable_autostart.sh
+++ home/private_dot_config/openbox/executable_autostart.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-# open polybar
+# Launch polybar
 ~/.config/polybar/launch.sh &
+
+# You can add new exec commands here or just create new .desktop files in the
+# ~/.config/autostart directory.
