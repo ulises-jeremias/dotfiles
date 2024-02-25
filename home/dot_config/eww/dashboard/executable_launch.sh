4fd147245be9a04ba32d0a552802403b53749ec2
ulises-jeremias <ulisescf.24@gmail.com>
2024-02-25 15:13:28 -0300

    Refactor eww powermenu and dashboard launch scripts and window definitions

--- home/dot_config/eww/dashboard/executable_launch.sh
+++ home/dot_config/eww/dashboard/executable_launch.sh
@@ -1,71 +1,39 @@
 #!/usr/bin/env bash
 
 ## Files and cmd
-FILE="$HOME/.cache/eww_launch.dashboard"
 CFG="$HOME/.config/eww/dashboard"
 
 ## Run eww daemon if not running already
-if [[ ! $(pidof eww) ]]; then
+if [[ "$(eww ping)" != "pong" ]]; then
 	eww daemon
-	# wait for eww daemon to start
-	max=10
-	for _ in $(seq 1 $max); do
-		if [[ $(pidof eww) ]]; then
-			break
-		elif [[ $_ -eq $max ]]; then
-			notify-send "Eww daemon failed to start" -u critical
-			exit 1
-		fi
-		sleep 0.1
-	done
+	if [[ "$(eww ping)" != "pong" ]]; then
+		notify-send "Eww" "Failed to start eww daemon" -u critical
+		exit 1
+	fi
 fi
 
 ## Open widgets
 run_eww() {
-	eww --config "$CFG" open-many \
-		background \
-		profile \
-		system \
-		clock \
-		uptime \
-		music \
-		github \
-		reddit \
-		twitter \
-		youtube \
-		weather \
-		apps \
-		mail \
-		logout \
-		sleep \
-		reboot \
-		poweroff \
-		folders
+	eww --config "$CFG" open-many --toggle \
+		dashboard-background \
+		dashboard-profile \
+		dashboard-system \
+		dashboard-clock \
+		dashboard-uptime \
+		dashboard-music \
+		dashboard-github \
+		dashboard-reddit \
+		dashboard-twitter \
+		dashboard-youtube \
+		dashboard-weather \
+		dashboard-apps \
+		dashboard-mail \
+		dashboard-logout \
+		dashboard-sleep \
+		dashboard-reboot \
+		dashboard-poweroff \
+		dashboard-folders
 }
 
 ## Launch or close widgets accordingly
-if [[ ! -f "$FILE" ]]; then
-	touch "$FILE"
-	run_eww
-else
-	eww --config "$CFG" close \
-		background \
-		profile \
-		system \
-		clock \
-		uptime \
-		music \
-		github \
-		reddit \
-		twitter \
-		youtube \
-		weather \
-		apps \
-		mail \
-		logout \
-		sleep \
-		reboot \
-		poweroff \
-		folders
-	rm "$FILE"
-fi
+run_eww
--- home/dot_config/eww/powermenu/executable_launch.sh
+++ home/dot_config/eww/powermenu/executable_launch.sh
@@ -1,51 +1,29 @@
 #!/usr/bin/env bash
 
 ## Files and cmd
-FILE="$HOME/.cache/eww_launch.powermenu"
 CFG="$HOME/.config/eww/powermenu"
 
 ## Run eww daemon if not running already
-if [[ ! $(pidof eww) ]]; then
+if [[ "$(eww ping)" != "pong" ]]; then
 	eww daemon
-	# wait for eww daemon to start
-	max=10
-	for _ in $(seq 1 $max); do
-		if [[ $(pidof eww) ]]; then
-			break
-		elif [[ $_ -eq $max ]]; then
-			notify-send "Eww daemon failed to start" -u critical
-			exit 1
-		fi
-		sleep 0.1
-	done
+	if [[ "$(eww ping)" != "pong" ]]; then
+		notify-send "Eww" "Failed to start eww daemon" -u critical
+		exit 1
+	fi
 fi
 
 ## Open widgets
 run_eww() {
-	eww --config "$CFG" open-many \
-		background \
-		close \
-		clock \
-		uptime \
-		logout \
-		sleep \
-		reboot \
-		poweroff
+	eww --config "$CFG" open-many --toggle \
+		powermenu-background \
+		powermenu-close \
+		powermenu-clock \
+		powermenu-uptime \
+		powermenu-logout \
+		powermenu-sleep \
+		powermenu-reboot \
+		powermenu-poweroff
 }
 
 ## Launch or close widgets accordingly
-if [[ ! -f "$FILE" ]]; then
-	touch "$FILE"
-	run_eww
-else
-	eww --config "$CFG" close \
-		background \
-		close \
-		clock \
-		uptime \
-		logout \
-		sleep \
-		reboot \
-		poweroff
-	rm "$FILE"
-fi
+run_eww
