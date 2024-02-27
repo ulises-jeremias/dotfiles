d0b3aebaccdd09efde79b6b1c9120c4e4315908c
ulises-jeremias <ulisescf.24@gmail.com>
2024-02-27 04:41:08 -0300

    Add ibus configuration and keybinding for screenshot

---
+++ home/dot_zsh/config.d/sgpt.zsh
@@ -0,0 +1,13 @@
+# Shell-GPT integration ZSH v0.2
+_sgpt_zsh() {
+if [[ -n "$BUFFER" ]]; then
+    _sgpt_prev_cmd=$BUFFER
+    BUFFER+="   "
+    zle -I && zle redisplay
+    BUFFER=$(sgpt --shell <<< "$_sgpt_prev_cmd" --no-interaction)
+    zle end-of-line
+fi
+}
+zle -N _sgpt_zsh
+bindkey "^p" _sgpt_zsh
+# Shell-GPT integration ZSH v0.2
